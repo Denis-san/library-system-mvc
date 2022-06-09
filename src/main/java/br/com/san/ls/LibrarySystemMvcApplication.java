e21578f9a0701c43df3527f1abb19315f2a9b111
Denis-san <denilson.nt.nc@gmail.com>
2022-06-09 15:24:48 -0300

    project created

---
+++ src/main/java/br/com/san/ls/LibrarySystemMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.san.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/san/ls/LibrarySystemMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.san.ls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

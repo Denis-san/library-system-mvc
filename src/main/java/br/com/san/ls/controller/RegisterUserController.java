af2218f3b99a4806707e150e742e78e587b43a1e
Denis-san <denilson.nt.nc@gmail.com>
2022-07-01 12:49:15 -0300

    added feedback error messages to verifyPassword and verifyEmail fields

--- src/main/java/br/com/san/ls/controller/RegisterUserController.java
+++ src/main/java/br/com/san/ls/controller/RegisterUserController.java
@@ -4,11 +4,13 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.san.ls.dto.UserDTO;
 
@@ -26,10 +28,18 @@ public class RegisterUserController {
 	@PostMapping("/processRegister")
 	public ModelAndView processRegister(@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult bdResult) {
 		ModelAndView mv = new ModelAndView("/pages/user/register-user");
-		
+
 		if (!bdResult.hasErrors()) {
 			mv.setViewName("/pages/user/welcome_user");
 		} else {
+			for (FieldError error : bdResult.getFieldErrors()) {
+				if (error.getDefaultMessage().contains("emails")) {
+					mv.addObject("emailVerifyError", error.getDefaultMessage());
+				}
+				if (error.getDefaultMessage().contains("senhas")) {
+					mv.addObject("passwordVerifyError", error.getDefaultMessage());
+				}
+			}
 			mv.addObject("user", userDTO);
 		}
 

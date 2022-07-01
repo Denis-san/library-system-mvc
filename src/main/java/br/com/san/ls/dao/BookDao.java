ec0d3444e1752305ebb0eb97bbacb4b51a8b522f
Denis-san <denilson.nt.nc@gmail.com>
2022-07-01 17:07:06 -0300

    created repository classes

---
+++ src/main/java/br/com/san/ls/dao/BookDao.java
@@ -0,0 +1,11 @@
+package br.com.san.ls.dao;
+
+import java.util.List;
+
+import br.com.san.ls.entity.Book;
+
+public interface BookDao {
+	
+	public List<Book> search(String search);
+	
+}
---
+++ src/main/java/br/com/san/ls/dao/impl/BookDaoImpl.java
@@ -0,0 +1,30 @@
+package br.com.san.ls.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.san.ls.dao.BookDao;
+import br.com.san.ls.entity.Book;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	public List<Book> search(String search) {
+		
+		TypedQuery<Book> query = entityManager.createQuery("SELECT b FROM Book WHERE LOWER(b.title) LIKE :search ORDER BY b.title", Book.class);
+		query.setParameter("search", "%" + search + "%");
+
+		return query.getResultList();
+		
+	}
+
+}
